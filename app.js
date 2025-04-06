3ad1e00c29edf10896b9ace7cffed6a7f9c4f60d
yehuda3h <yehuda3h@gmail.com>
2025-04-06 14:48:05 +0300

    1234

---
+++ app.js
@@ -0,0 +1,28 @@
+//output
+console.log("hello world")
+//int x =5.....
+//let x=5,const y=5.
+const x=5
+console.log(x)
+let firstname="oren"
+let age =6
+console.log(firstname)
+console.log(firstname+" " + "is"+" " +age + " years old ")
+let x1=Math.random()
+console.log(x1)
+let y1=Math.random() *100
+console.log(y1)
+
+let t1=Math.floor(Math.random() *100)
+console.log(t1)
+
+
+
+
+
+
+
+
+
+
+
